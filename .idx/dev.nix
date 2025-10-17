3b21d3a1038742da3801700e814ff90988ef1539
naufal rahmadan <naufalrahmadan12@gmail.com>
2025-10-17 17:11:43 +0000

    Initial commit

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -6,7 +6,10 @@
   # Use https://search.nixos.org/packages to find packages
   packages = [
     pkgs.nodejs_20
+    pkgs.postgresql
+    pkgs.openssl
     pkgs.zulu
+    pkgs.sudo
   ];
   # Sets environment variables in the workspace
   env = {};
